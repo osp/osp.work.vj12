af19ca4f5a1e6ae33efe7f8a08430c9723950aea
Stéphanie Vilayphiou <stephanie@stdin.fr>
2011-11-24 16:00:22 +0100

    added common header to paracode + renamed dominokingdom folder into paracode

--- paracode/_attachments/js/paracode.js
+++ paracode/_attachments/js/paracode.js
@@ -82,7 +82,7 @@ $(document).ready(function() {
     }
 
     function getLinks (id) {
-        db.view('dominokindm/foo?startkey=["' + id + '"]&endkey=["' + id + '", {}]', {  
+        db.view('vj12/foo?startkey=["' + id + '"]&endkey=["' + id + '", {}]', {  
             success: function(data) {  
                 // appends links/comments if any
                 if (data.rows.length !== 0) {
@@ -240,7 +240,7 @@ $(document).ready(function() {
             {
             success: function() { 
                 // Updates the color on the commented elements
-                db.view('dominokindm/bar?group=true&startkey="' + ids[0] + '"&endkey="' + ids[(ids.length - 1)] + '"', {  
+                db.view('vj12/bar?group=true&startkey="' + ids[0] + '"&endkey="' + ids[(ids.length - 1)] + '"', {  
                     success: function(data) {  
                         for (i in data.rows) {
                             var key = data.rows[i].key;
@@ -259,7 +259,7 @@ $(document).ready(function() {
     });
 
     // puts the color on all the elements 
-    db.view('dominokindm/bar?group=true', {  
+    db.view('vj12/bar?group=true', {  
         success: function(data) {  
             // Sets tint
             for (i in data.rows) {
@@ -270,7 +270,7 @@ $(document).ready(function() {
             }
         }
     });  
-    db.view('dominokindm/count_links?group=true', {  
+    db.view('vj12/count_links?group=true', {  
         success: function(data) {  
             $("p#comment_count span").html(data.rows.length);
         }
